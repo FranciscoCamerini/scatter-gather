8a22e0b1f27c430ca291a1e48b4aedf5330d4682
Francisco Camerini <franciscocamerini@onsign.tv>
2024-06-11 15:46:04 -0300

    Improve run script

--- run.sh
+++ run.sh
@@ -3,10 +3,15 @@
 SERVER_PORT=8080
 CRAWLER_PORTS=(8081 8082 8083 8084)
 
-make
+BASE_DIR=$(dirname "$0")
+STORAGE_DIR="$BASE_DIR/storage" # Used for storing PID's
 
-./master/master -pidfile=storage/.master.pid -crawlers=$(IFS=,; echo "${CRAWLER_PORTS[*]}")&
+mkdir -p "$STORAGE_DIR"
+
+make -C "$BASE_DIR"
+
+"$BASE_DIR/master/master" -pidfile="$STORAGE_DIR/.master.pid" -crawlers=$(IFS=,; echo "${CRAWLER_PORTS[*]}") &
 
 for port in "${CRAWLER_PORTS[@]}"; do
-    ./crawler/crawler -port=$port -pidfile=storage/.crawler-$port.pid&
+    "$BASE_DIR/crawler/crawler" -port="$port" -pidfile="$STORAGE_DIR/.crawler-$port.pid" &
 done
